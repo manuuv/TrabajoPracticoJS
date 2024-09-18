830e6f12e117e9cdb4499514d495609b3637668e
Manuel Vazquez <manuuva5@gmail.com>
2024-09-18 10:47:50 -0300

    Ejercicio 2.2

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-let a = 5;
-let b = 6; 
-let c = 14;
-let mayor = Math.max(a, b, c);
-console.log("El mayor de los tres números es: " + mayor);
-
+let a = prompt("Ingresa un número y te digo si es par o impar")
+a % 2 == 0 ? console.log("El número "+a+", es par") : console.log("El número "+a+", es impar");
