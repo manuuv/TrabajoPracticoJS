dbf8cebb393a8bea26e34a31212258b3ed5d98d6
Manuel Vazquez <manuuva5@gmail.com>
2024-10-09 09:37:29 -0300

    Ejercicio 6.1

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-let libro1 ={
-    titulo: "El Quijote",
-    año: 1605,
-    autor: "Miguel de Cervantes Saavedra"
-}
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numerosx2 = numeros.map((elemento) => {
+    return elemento * 2;
+})
 
-console.log(libro1);
-
-let fechaActual = new Date();
-let añoActual = fechaActual.getFullYear();
-const esAntiguo = (libro) => {
-    let antiguo =  (añoActual - libro.año) < 10 ? false : true;
-    return antiguo;
-}
-
-console.log("El libro "+libro1.titulo+" es antiguo: "+ esAntiguo(libro1));
+console.log("Números originales: " + numeros.join(', '))
+console.log("Números nultiplicados por 2: " + numerosx2.join(', '))
