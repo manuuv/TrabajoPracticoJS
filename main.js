e02d948fde9f4a2f39f475a845f9fb3206208374
Manuel Vazquez <manuuva5@gmail.com>
2024-10-09 12:32:45 -0300

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const texto = document.getElementById("texto");
+    const mensaje = document.getElementById("mensaje");
     const boton1 = document.getElementById("boton1");
     const boton2 = document.getElementById("boton2");
 
-    let deshabiitado = false;
 
     if (texto && boton1 && boton2) {
         
-        boton1.addEventListener('click', (event) => {
-            if (deshabiitado == false) {
-                texto.disabled = true;
-                deshabiitado = true;
+        boton1.addEventListener("click", () => {
+            const email = texto.value.trim();
+            if (email) {
+                localStorage.setItem("datos", JSON.stringify({ texto: email }));
+                mensaje.textContent = `Correo electrónico guardado: ${email}`;
+            } else {
+                mensaje.textContent = "Por favor, ingrese un correo electrónico válido.";
             }
-        });
+        })
 
-        boton2.addEventListener('click', (event) => {
-            if (deshabiitado == true) {
-                texto.disabled = false;
-                deshabiitado = false;
+        boton2.addEventListener("click", () => {
+            const correo = localStorage.getItem("datos");
+            if (correo) {
+                localStorage.removeItem("datos");
+                mensaje.textContent = "Correo electrónico borrado.";
+            } else {
+                mensaje.textContent = "No existe el correo.";
             }
-        });
+        })
     }
 });
